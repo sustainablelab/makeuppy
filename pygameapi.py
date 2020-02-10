63f59b8680544c1815552e88792e1d23a84ff9e5
sustainablelab <sustainablelab@gmail.com>
2020-02-10 17:35:10 -0500

    Dumb commit to change computers

--- pygameapi.py
+++ pygameapi.py
@@ -1,3 +1,16 @@
+"""API of common functions when writing a GUI with pygame
+SEE
+    https://www.pygame.org/docs/
+"""
+import pygame
+
 def print_is_ok():
     """Is OK."""
     print("Module import is OK.")
+def user_quit(event, key_pressed):
+    """Return True if user quit.
+    `event`:
+        list item from iterating over pygame.event.get()
+    `key_pressed`:
+        list returned by pygame.key.get_pressed()
+        index list with constants such as pygame.K_q
