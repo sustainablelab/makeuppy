be6df8722f6af2d8a1eb37bdecbf7502c5d110d0
sustainablelab <sustainablelab@gmail.com>
2020-02-10 23:06:45 -0500

    Start unit tests for pgui.user_quit()

--- pygameapi.py
+++ pygameapi.py
@@ -1,21 +1,34 @@
 import pygame
 
-def print_is_ok():
-    """Is OK."""
-    print("Module import is OK.")
 def user_quit(event, key_pressed):
-    """Return True if user quit.
-    event:
-        - a message from the message list
-          returned by pygame.event.get()
-    key_pressed:
-        - array of booleans returned by pygame.key.get_pressed()
-        - use key constant values to index the array
-          example: key_pressed[pygame.K_q]
-    USAGE:
-        quit=False
-        while not quit:
-            for event in pygame.event.get():
-                key_pressed = pygame.key.get_pressed()
-                quit = user_quit(event, key_pressed)
     """
+    Returns True if user quit.
+    User quits by clicking the red x or by keyboard shortcut.
+
+    Parameters
+    ----------
+    'event'
+        A message from the message list returned by
+        'pygame.event.get()'.
+        Quit if 'event.type' is 'pygame.QUIT'.
+    'key_pressed'
+        Boolean array returned by 'pygame.key.get_pressed()'.
+        Use key constant values to index the array.
+        Example: key_pressed[pygame.K_q]
+
+    Examples
+    --------
+    quit=False
+    while not quit:
+        for event in pygame.event.get():
+            key_pressed = pygame.key.get_pressed()
+            quit = user_quit(event, key_pressed)
+    """
+    return(_user_clicked_red_x(event)
+        or _user_pressed_q(key_pressed)
+        )
+
+def _user_clicked_red_x(event): return event.type == pygame.QUIT
+
+# TODO: set package attribute to turn this 'dev' shortcut off.
+def _user_pressed_q(key_pressed): return key_pressed[pygame.K_q]
--- test_pygameapi.py
+++ test_pygameapi.py
@@ -4,10 +4,52 @@ VIM USAGE:
     ;ut -- run unit tests for pkgname
         - requires active buffer is test_pkgname.py
 """
+import pygameapi as pgui # -- module under test
+# unit test framework
 import unittest
-import pygameapi
+import pygame
+# use namedtuple to fake pygame function return values
+from collections import namedtuple
+# use numpy to fake pygame function return values
+import numpy as np
 
-class pygameapi_unittest_PlumbingIsOK(unittest.TestCase):
-    """Check the plumbing."""
-    def test_plumbing(self):
-        self.assertTrue(True)
+class user_quit(unittest.TestCase):
+    def setUp(self):
+        # ---Fake syntax for a pygame event---
+        # Usage:
+        #   event = self.Event(type=pygame.QUIT)
+        self.Event = namedtuple('FakePygameEvent', ['type'])
+        #
+        # ---Fake syntax for pygame boolean array of key presses---
+        # Fake key_pressed==False as [T, T, T, ..., T, F]
+        # Usage:
+        #   key_pressed = self.key_pressed != pygame.K_x
+        # Fake key_pressed==True  as [F, F, F, ..., F, T]
+        # Usage:
+        #   key_pressed = self.key_pressed == pygame.K_x
+        self.key_pressed = np.arange(1+pygame.K_q)
+
+    def test_Returns_True_if_user_clicks_red_x(self):
+        # Fake event: click red x
+        event = self.Event(type=pygame.QUIT)
+        # Fake key_pressed: do not press q
+        key_pressed = self.key_pressed != pygame.K_q
+        # =====[ Operate ]=====
+        self.assertTrue(pgui.user_quit(event, key_pressed))
+
+    def test_Returns_True_if_user_presses_q(self):
+        # Fake event: do not click red x
+        event = self.Event(type=pygame.QUIT+42)
+        # Fake key_pressed: press q
+        key_pressed = self.key_pressed == pygame.K_q
+        # =====[ Operate ]=====
+        self.assertTrue(pgui.user_quit(event, key_pressed))
+
+    def test_Returns_False_if_user_does_not_quit(self):
+        # Fake event: do not click red x
+        event = self.Event(type=pygame.QUIT+42)
+        # Fake key_pressed: do not press q
+        key_pressed = self.key_pressed != pygame.K_q
+        # =====[ Operate ]=====
+        self.assertFalse(pgui.user_quit(event, key_pressed))
+
