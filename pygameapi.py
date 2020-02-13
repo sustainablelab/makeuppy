a1adfd2b06ec12358a0b044d775ca4e9536237d3
sustainablelab <sustainablelab@gmail.com>
2020-02-13 11:58:28 -0500

    Dumb commit to switch computers

--- pygameapi.py
+++ pygameapi.py
@@ -1,9 +1,51 @@
 import pygame
+from collections import namedtuple
 
-def user_quit(event, key_pressed):
+DEV = False
+def get_dev_mode(): return DEV
+def dev_mode(onoff_flag=True):
+    """Enable development mode functionality.
+
+    Behavior
+    --------
+    pgui.DEV is False if dev_mode is called with False
+    pgui.DEV is False if dev_mode is not called
+    pgui.DEV is True if dev_mode is called with True
+    pgui.DEV is True if dev_mode is called with no argument
+
+    Examples
+    --------
+    1. Disable dev mode
+    import pygameapi as pgui
+    # do nothing
+
+    2. Enable dev mode
+    import pygameapi as pgui
+    pgui.dev_mode()         # Turn on dev mode
+    # or
+    pgui.dev_mode(True)     # Turn on dev mode
+
+    3. Disable dev mode *after* it is enabled
+    import pygameapi as pgui
+    pgui.dev_mode(True)     # Turn on dev mode
+    # ...some time later in the program...
+    pgui.dev_mode(False)    # Turn off dev mode
+
     """
-    Returns True if user quit.
-    User quits by clicking the red x or by keyboard shortcut.
+    global DEV
+    DEV = onoff_flag
+
+def user_quit(event, key_pressed, key_mods):
+    """
+    Returns True if user clicks window-close or shortcut-key quits.
+
+    Behavior
+    --------
+    Returns False if user does not quit
+    Returns False if user presses q with dev mode disabled
+    Returns True if user clicks red x
+    Returns True if user presses Ctrl q
+    Returns True if user presses q in dev mode
 
     Parameters
     ----------
@@ -15,20 +57,166 @@ def user_quit(event, key_pressed):
         Boolean array returned by 'pygame.key.get_pressed()'.
         Use key constant values to index the array.
         Example: key_pressed[pygame.K_q]
+    'key_mods'
+        Bitmask of all modifier keys held down.
+        Returned by 'pygame.key.get_mods()'.
+        Use bitwise operators to test if keys are held.
+        Example: key_mods & pygame.KMOD_CTRL
 
     Examples
     --------
-    quit=False
+    import pygameapi as pgui
+    pygame.init()
+    display = pygame.display.set_mode( (100,100) )
+    quit = False
     while not quit:
         for event in pygame.event.get():
             key_pressed = pygame.key.get_pressed()
             quit = user_quit(event, key_pressed)
     """
     return(_user_clicked_red_x(event)
-        or _user_pressed_q(key_pressed)
+        or  (_user_held_Ctrl(key_mods)
+            and _user_pressed_q(key_pressed)
+            )
+        or _dev( _user_pressed_q(key_pressed) )
         )
+
+def user_opens_cmdline(key_pressed, key_mods):
+    """
+    Behavior
+    --------
+    Returns false if user does not press colon
+    Returns true if user presses colon
+    """
+    return _user_pressed_colon(key_pressed, key_mods)
+
+def user_closes_cmdline(key_pressed):
+    """
+    Behavior
+    --------
+    Returns False if user does not press Esc
+    Returns True if user presses Esc
+    """
+    return _user_pressed_Esc(key_pressed)
+
+Window = namedtuple('Window', [ 'cols', 'rows' ])
+
+def window_size(win):
+    """Return size of Window 'win' as tuple (width, height).
+
+    Behavior
+    --------
+    Window is a namedtuple with field names(cols,rows)
+    Returns tuple width,height
+
+    Parameters
+    ----------
+    'win'
+        type(win) is namedtuple 'pygameapi.Window'
+        Example: win = Window( cols=1200, rows=600 )
+    """
+    return (win.cols, win.rows)
+
+# Credit color scheme to Steve Losh, author of badwolf.vim
+_badwolf_color_names = [
+    # 'name'            (R,G,B)           ['hex', 256-color-term]
+    # Normal text.
+    'plain'         , # (248,246,242)   = ['f8f6f2', 15]
+    # Pure and simple.
+    'snow'          , # (255,255,255)   = ['ffffff', 15]
+    'coal'          , # (0,0,0)         = ['000000', 16]
+    # Gravel colors based on a brown from Clouds Midnight.
+    'brightgravel'  , # (217,206,195)   = ['d9cec3', 252]
+    'lightgravel'   , # (153,143,132)   = ['998f84', 245]
+    'gravel'        , # (133,127,120)   = ['857f78', 243]
+    'mediumgravel'  , # (102,100,98)    = ['666462', 241]
+    'deepgravel'    , # (69,65,59)      = ['45413b', 238]
+    'deepergravel'  , # (53,50,45)      = ['35322d', 236]
+    'darkgravel'    , # (36,35,33)      = ['242321', 235]
+    'blackgravel'   , # (28,27,26)      = ['1c1b1a', 233]
+    'blackestgravel', # (20,20,19)      = ['141413', 232]
+    # From highlight in photo of a glass of Dale's Pale Ale on my desk.
+    'dalespale'     , # (250,222,62)    = ['fade3e', 221]
+    # A beautiful tan from Tomorrow Night.
+    'dirtyblonde'   , # (244,207,134)   = ['f4cf86', 222]
+    # Delicious, chewy red from Made of Code for poppiest highlights.
+    'taffy'         , # (255,44,75)     = ['ff2c4b', 196]
+    # Another chewy accent, but use sparingly!
+    'saltwatertaffy', # (140,255,186)   = ['8cffba', 121]
+    # Use for things that denote 'where the user is'
+    'tardis',         # (10,157,255)    = ['0a9dff', 39]
+    # This one's from Mustang, not Florida!
+    'orange',         # (255,167,36)    = ['ffa724', 214]
+    # A limier green from Getafe.
+    'lime',           # (174,238,0)     = ['aeee00', 154]
+    # Rose's dress in The Idiot's Lantern.
+    'dress',          # (255,158,184)   = ['ff9eb8', 211]
+    # Another play on the brown from Clouds Midnight.
+    'toffee',         # (184,136,83)    = ['b88853', 137]
+    # Also based on that Clouds Midnight brown.
+    'coffee',         # (199,145,91)    = ['c7915b', 173]
+    'darkroast'       # (136,99,63)     = ['88633f', 95]
+    ]
+_badwolf_color_values = (
+    # Normal text.
+    (248,246,242),
+    # Pure and simple.
+    (255,255,255),
+    (0,0,0),
+    # Gravel colors based on a brown from Clouds Midnight.
+    (217,206,195),
+    (153,143,132),
+    (133,127,120),
+    (102,100,98),
+    (69,65,59),
+    (53,50,45),
+    (36,35,33),
+    (28,27,26),
+    (20,20,19),
+    # From highlight in photo of a glass of Dale's Pale Ale on my desk.
+    (250,222,62),
+    # A beautiful tan from Tomorrow Night.
+    (244,207,134),
+    # Delicious, chewy red from Made of Code for poppiest highlights.
+    (255,44,75),
+    # Another chewy accent, but use sparingly!
+    (140,255,186),
+    # Use for things that denote 'where the user is'
+    (10,157,255),
+    # This one's from Mustang, not Florida!
+    (255,167,36),
+    # A limier green from Getafe.
+    (174,238,0),
+    # Rose's dress in The Idiot's Lantern.
+    (255,158,184),
+    # Another play on the brown from Clouds Midnight.
+    (184,136,83),
+    # Also based on that Clouds Midnight brown.
+    (199,145,91),
+    (136,99,63)
+    )
+Color = namedtuple(
+    'Color',
+    _badwolf_color_names,
+    defaults=_badwolf_color_values
+    )
+
+# ---Helpers---
+def _dev(condition): return DEV and condition
 
 def _user_clicked_red_x(event): return event.type == pygame.QUIT
 
-# TODO: set package attribute to turn this 'dev' shortcut off.
 def _user_pressed_q(key_pressed): return key_pressed[pygame.K_q]
+
+def _user_pressed_semicolon(key_pressed): return key_pressed[pygame.K_SEMICOLON]
+
+def _user_pressed_Esc(key_pressed): return key_pressed[pygame.K_ESCAPE]
+
+def _user_held_Ctrl(key_mods): return key_mods & pygame.KMOD_CTRL
+
+def _user_held_Shift(key_mods): return key_mods & pygame.KMOD_SHIFT
+
+def _user_pressed_colon(key_pressed, key_mods): 
+    return (_user_held_Shift(key_mods)
+            and _user_pressed_semicolon(key_pressed)
+            )
--- test_pygameapi.py
+++ test_pygameapi.py
@@ -3,53 +3,237 @@
 VIM USAGE:
     ;ut -- run unit tests for pkgname
         - requires active buffer is test_pkgname.py
+        - edit $MYVIMRC to select unittest or pytest
+        - see https://docs.pytest.org/en/latest/usage.html#cmdline
 """
 import pygameapi as pgui # -- module under test
+# from pygameapi import _get_dev_mode
+from pygameapi import dev_mode
 # unit test framework
 import unittest
+from unittest.mock import patch
 import pygame
 # use namedtuple to fake pygame function return values
 from collections import namedtuple
 # use numpy to fake pygame function return values
 import numpy as np
 
+class dev_mode(unittest.TestCase):
+    def setUp(self):
+        """Restore default state of pygameapi.
+        
+        Context
+        -------
+        pygameapi contains the line:
+        DEV = False
+
+        Intent
+        ------
+        Reset DEV between tests.
+        DEV is global.
+        Call dev_mode(True) changes DEV to True.
+        """
+        pgui.DEV=False
+
+    def test_pgui_DOT_DEV_is_False_if_dev_UNDERSCORE_mode_is_not_called(self):
+        """This test is only for generating the docstring.
+        I'm not sure how to test this correctly.
+        
+        Issue
+        -----
+        Test passes even if I edit pygameapi to DEV=True.
+        
+        Cause
+        -----
+        Test setUp overrides pygameapi with pgui.DEV=False.
+        """
+        self.assertFalse(pgui.DEV)
+
+    def test_pgui_DOT_DEV_is_True_if_dev_UNDERSCORE_mode_is_called_with_True(self):
+        pgui.dev_mode(True)
+        self.assertTrue(pgui.get_dev_mode())
+
+    def test_pgui_DOT_DEV_is_True_if_dev_UNDERSCORE_mode_is_called_with_no_argument(self):
+        pgui.dev_mode()
+        self.assertTrue(pgui.get_dev_mode())
+
+    def test_pgui_DOT_DEV_is_False_if_dev_UNDERSCORE_mode_is_called_with_False(self):
+        # =====[ Setup ]=====
+        pgui.dev_mode(True)
+        # =====[ Operate ]=====
+        pgui.dev_mode(False)
+        # =====[ Test ]=====
+        self.assertFalse(pgui.get_dev_mode())
+
 class user_quit(unittest.TestCase):
     def setUp(self):
-        # ---Fake syntax for a pygame event---
-        # Usage:
-        #   event = self.Event(type=pygame.QUIT)
+        """
+        Default all tests to dev mode disabled
+        --------------------------------------
+        Usage:
+            # Test dev mode functionality by enabling dev mode
+            dev_mode()
+
+        Fake syntax for a pygame 'event'
+        --------------------------------
+        Usage:
+            event = self.Event(type=pygame.QUIT)
+
+        Fake syntax for pygame 'boolean array of key presses'
+        -----------------------------------------------------
+        Usage:
+            # Fake 'key_pressed==False' like this:
+            # key_pressed = [T, T, T, ..., T, F]
+            key_pressed = self.key_pressed != pygame.K_x
+        Usage:
+            # Fake 'key_pressed==True' like this:
+            # key_pressed = [F, F, F, ..., F, T]
+            key_pressed = self.key_pressed == pygame.K_x
+
+        Fake modifier keys
+        ------------------
+        Usage:
+            # Fake modifier keys held down: none
+            key_mods = pygame.key.get_mods()
+        Usage
+            # Fake modifier keys held down: Ctrl
+            pygame.key.set_mods(pygame.KMOD_CTRL)
+            key_mods = pygame.key.get_mods()
+        """
+        # dev mode disabled
+        pgui.dev_mode(False)
+
+        # define event.type datatype for faking events
         self.Event = namedtuple('FakePygameEvent', ['type'])
-        #
-        # ---Fake syntax for pygame boolean array of key presses---
-        # Fake key_pressed==False as [T, T, T, ..., T, F]
-        # Usage:
-        #   key_pressed = self.key_pressed != pygame.K_x
-        # Fake key_pressed==True  as [F, F, F, ..., F, T]
-        # Usage:
-        #   key_pressed = self.key_pressed == pygame.K_x
-        self.key_pressed = np.arange(1+pygame.K_q)
+
+        # define array for faking key presses by Boolean comparison
+        hope_pygame_K_upper_limit = 300
+        self.key_pressed = np.arange(1+hope_pygame_K_upper_limit)
+
+        # Must init to call pygame.key.get_mods() during tests
+        pygame.init()
+
+        # no key modifiers are being held down
+        pygame.key.set_mods(pygame.KMOD_NONE)
 
     def test_Returns_True_if_user_clicks_red_x(self):
+        # =====[ Setup ]=====
+
         # Fake event: click red x
         event = self.Event(type=pygame.QUIT)
+
         # Fake key_pressed: do not press q
         key_pressed = self.key_pressed != pygame.K_q
-        # =====[ Operate ]=====
-        self.assertTrue(pgui.user_quit(event, key_pressed))
 
-    def test_Returns_True_if_user_presses_q(self):
+        # Fake key_mods: none
+        key_mods = pygame.key.get_mods()
+
+        # =====[ Operate ]=====
+        self.assertTrue(pgui.user_quit(event, key_pressed, key_mods))
+
+    def test_Returns_False_if_user_presses_q_with_dev_mode_disabled(self):
+        # =====[ Setup ]=====
+
+        # Fake event: do not click red x
+        event = self.Event(type=pygame.QUIT+42)
+
+        # Fake key_pressed: press q
+        key_pressed = self.key_pressed == pygame.K_q
+
+        # Fake key_mods: none
+        key_mods = pygame.key.get_mods()
+
+        # =====[ Operate ]=====
+        self.assertFalse(pgui.user_quit(event, key_pressed, key_mods))
+
+    def test_Returns_True_if_user_presses_q_in_dev_mode(self):
+        # =====[ Setup ]=====
+
+        # Enable dev mode
+        pgui.dev_mode()
+
+        # Fake event: do not click red x
+        event = self.Event(type=pygame.QUIT+42)
+
+        # Fake key_pressed: press q
+        key_pressed = self.key_pressed == pygame.K_q
+
+        # Fake key_mods: none
+        key_mods = pygame.key.get_mods()
+
+        # =====[ Operate ]=====
+        self.assertTrue(pgui.user_quit(event, key_pressed, key_mods))
+
+    def test_Returns_True_if_user_presses_Ctrl_q(self):
         # Fake event: do not click red x
         event = self.Event(type=pygame.QUIT+42)
         # Fake key_pressed: press q
         key_pressed = self.key_pressed == pygame.K_q
+        # Fake key_mods: hold Ctrl
+        pygame.key.set_mods(pygame.KMOD_CTRL)
+        key_mods = pygame.key.get_mods()
         # =====[ Operate ]=====
-        self.assertTrue(pgui.user_quit(event, key_pressed))
+        self.assertTrue(pgui.user_quit(event, key_pressed, key_mods))
 
     def test_Returns_False_if_user_does_not_quit(self):
         # Fake event: do not click red x
         event = self.Event(type=pygame.QUIT+42)
         # Fake key_pressed: do not press q
         key_pressed = self.key_pressed != pygame.K_q
+        # Fake key_mods: none
+        key_mods = pygame.key.get_mods()
         # =====[ Operate ]=====
-        self.assertFalse(pgui.user_quit(event, key_pressed))
+        self.assertFalse(pgui.user_quit(event, key_pressed, key_mods))
 
+class user_opens_cmdline(unittest.TestCase):
+    def setUp(self):
+        # define array for faking key presses by Boolean comparison
+        self.key_pressed = np.arange(1+pygame.K_SEMICOLON)
+
+        # Must init to call pygame.key.get_mods() during tests
+        pygame.init()
+
+        # no key modifiers are being held down
+        pygame.key.set_mods(pygame.KMOD_NONE)
+
+    def test_Returns_true_if_user_presses_colon(self):
+        # =====[ Setup ]=====
+        # Fake user pressing colon
+        # Fake by pressing SEMICOLON
+        key_pressed = self.key_pressed == pygame.K_SEMICOLON
+        # while holding SHIFT
+        pygame.key.set_mods(pygame.KMOD_SHIFT)
+        key_mods = pygame.key.get_mods()
+        # =====[ Operate and Test ]=====
+        self.assertTrue(pgui.user_opens_cmdline(key_pressed, key_mods))
+
+    def test_Returns_false_if_user_does_not_press_colon(self):
+        key_pressed = self.key_pressed != pygame.K_SEMICOLON
+        key_mods = pygame.key.get_mods()
+        self.assertFalse(pgui.user_opens_cmdline(key_pressed, key_mods))
+
+class user_closes_cmdline(unittest.TestCase):
+    def setUp(self):
+        # define array for faking key presses by Boolean comparison
+        self.key_pressed = np.arange(1+pygame.K_ESCAPE)
+
+    def test_Returns_True_if_user_presses_Esc(self):
+        key_pressed = self.key_pressed == pygame.K_ESCAPE
+        self.assertTrue(pgui.user_closes_cmdline(key_pressed))
+        
+    def test_Returns_False_if_user_does_not_press_Esc(self):
+        key_pressed = self.key_pressed != pygame.K_ESCAPE
+        self.assertFalse(pgui.user_closes_cmdline(key_pressed))
+
+class Window(unittest.TestCase):
+    def test_Window_is_a_namedtuple_with_field_names_OPENPAREN_cols_COMMA_rows_CLOSEPAREN(self):
+        self.assertEqual(pgui.Window._fields[0], 'cols')
+        self.assertEqual(pgui.Window._fields[1], 'rows')
+
+class window_size(unittest.TestCase):
+    def test_Returns_tuple_width_COMMA_height(self):
+        # =====[ Setup ]=====
+        nc=654; nr=321
+        win = pgui.Window( cols=nc, rows=nr )
+        # =====[ Operate ]=====
+        self.assertEqual( pgui.window_size(win), (nc,nr) )
